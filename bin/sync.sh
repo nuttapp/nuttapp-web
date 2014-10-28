262794a2b46ec6879076b56561d6887ad9118951
Greg Ingelmo <git@ingelmo.org>
2014-10-28 17:01:35 -0400

    Update sync script to exclude dirs

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-clear && rsync -v -c -z -r --delete --exclude .git --exclude .DS_Store . na:/data/nuttapp/nuttapp-web | grep -i -E "\w*\.[a-z]{2,4}}?"
+clear && rsync -v -c -z -r --delete --exclude .git --exclude .DS_Store --exclude node_modules --exclude bower_components . na:/data/nuttapp/nuttapp-web | grep -i -E "\w*\.[a-z]{2,4}}?"
 
 
