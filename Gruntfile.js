efd66489e12135ed70bac896e3115876a9c34d27
Greg Ingelmo <git@ingelmo.org>
2015-06-14 10:26:27 -0400

    Create dev/prod specific builds w/ grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,76 +1,124 @@
 module.exports = function (grunt) {
+    var env = grunt.option('env') || 'dev';
+    var buildDir = 'builds/' + env;
+
     var gruntConfig = {
 
     watch: {
+      bootstrap: {
+        files: ['vendor/bootstrap/less/*.less'],
+        tasks: ['shell:buildBootstrap', 'copy:bootstrap_css_dev']
+      },
       less: {
-        files: ['less/*.less', 'vendor/bootstrap/less/*.less', '*.html'],
-        tasks: ['shell:compileBootstrap', 'less:compileSite', 'cssmin:minSite', 'copy']
-        // tasks: ['less:compileSite', 'copy']
+        files: ['less/*.less'],
+        tasks: ['less:lessDev']
+      },
+      html: {
+        files: ['*.html'],
+        tasks: ['copy:html']
+      },
+      images: {
+        files: ['images/*', 'css/images/*'],
+        tasks: ['copy:images', 'copy:cssImages']
       }
     },
 
     less: {
-      compileSite: {
+      lessDev: {
         options: {
-          strictMath: true,
           sourceMap: true,
           outputSourceFiles: true,
           sourceMapURL: 'site.css.map',
-          sourceMapFilename: 'css/site.css.map'
-          // sourceMapBasePath: 'css'
+          sourceMapFilename: buildDir + '/css/site.css.map'
         },
         files: {
-          'css/site.css': 'less/site.less'
+          'builds/dev/css/site.css': 'less/site.less'
+        }
+      },
+      lessProd: {
+        files: {
+          'builds/prod/css/site.css': 'less/site.less'
         }
       }
     },
 
     cssmin: {
-      minSite: {
+      minProd: {
         files: {
-          'css/site.min.css': ['css/site.css']
-        }
-      },
-      combine: {
-        files: {
-        'public/css/site.css': ['path/to/input_one.css', 'path/to/input_two.css']
+          'builds/prod/css/site.css': 'builds/prod/css/site.css'
         }
       }
     },
 
     shell: {
-      compileBootstrap: {
+      buildBootstrap: {
         command: 'grunt --base vendor/bootstrap/ --gruntfile vendor/bootstrap/Gruntfile.js less:compileCore autoprefixer:core csscomb:dist cssmin:minifyCore'
       }
     },
 
     copy: {
-      bootstrap_css: {
+      cssImages: {
+        expand: true,
+        cwd: 'css/',
+        src: 'images/*',
+        dest: buildDir + '/css/',
+        filter: 'isFile',
+      },
+      bootstrap_css_dev: {
         expand: true,
         cwd: 'vendor/bootstrap/dist/css/',
         src: 'bootstrap.*css*',
-        dest: 'css/',
+        dest: buildDir + '/css/',
         filter: 'isFile',
         flatten: true
+      },
+      bootstrap_css_prod: {
+        src: 'vendor/bootstrap/dist/css/bootstrap.min.css',
+        dest: buildDir + '/css/bootstrap.css',
       },
       html: {
         expand: true,
         cwd: './',
         src: '*.html',
-        dest: 'public/',
+        dest: buildDir,
         filter: 'isFile',
         flatten: true
       },
-      css: {
+      images: {
         expand: true,
-        cwd: 'css/',
-        src: 'images/*',
-        dest: 'public/css/',
+        cwd: 'images',
+        src: '*',
+        dest: buildDir + '/images/',
         filter: 'isFile',
+        flatten: true
       }
-    }
+    },
+   
+    clean: {
+        dev: 'builds/dev/*',
+        prod: 'builds/prod/*'
+    } 
   };
-  
+
+  if (env === 'dev') {
+    grunt.registerTask('build', [
+        'clean:dev',
+        'shell:buildBootstrap', 'copy:bootstrap_css_dev',
+        'less:lessDev',
+        'copy:html',
+        'copy:images', 'copy:cssImages'
+    ]);
+  } else if (env === 'prod') {
+    grunt.registerTask('build', [
+        'clean:prod',
+        'shell:buildBootstrap', 'copy:bootstrap_css_prod',
+        'less:lessProd',
+        'cssmin:minProd',
+        'copy:html',
+        'copy:images', 'copy:cssImages'
+    ]);
+  }
+
   require('time-grunt')(grunt);
   grunt.initConfig(gruntConfig);
 
